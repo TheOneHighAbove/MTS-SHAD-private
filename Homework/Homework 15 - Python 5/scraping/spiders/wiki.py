c4dabb7a472c8db32a9529dcdb79b8deffad7e62
Roman <roman.zakharov.d@gmail.com>
2026-01-26 14:03:06 +0300

    Refactor WikiSpider structure and logic
    Refactor WikiSpider to improve readability and maintainability.

--- Homework/Homework 15 - Python 5/scraping/spiders/wiki.py
+++ Homework/Homework 15 - Python 5/scraping/spiders/wiki.py
@@ -8,20 +8,16 @@ class WikiSpider(scrapy.Spider):
     start_urls = ['https://ru.wikipedia.org/wiki/Категория:Фильмы_по_алфавиту']
 
     # НАСТРОЙКА ОГРАНИЧЕНИЯ
-    # Установи число (например, 10) или строку 'All'
-    pages_limit = 10
-    pages_processed = 1  # Счетчик текущих страниц
+    pages_limit = 10 # Число страниц или All (все страницы)
+    pages_processed = 1  
 
     def parse(self, response):
-        # 1. Сбор ссылок на фильмы
         movie_links = response.css('#mw-pages .mw-category-group a::attr(href)').getall()
         for link in movie_links:
             yield response.follow(link, callback=self.parse_movie, priority=10)
 
-        # 2. Логика перехода с ограничителем
         next_page = response.xpath("//a[contains(text(), 'Следующая страница')]/@href").get()
 
-        # Проверяем: нужно ли идти дальше?
         should_continue = (
                 self.pages_limit == 'All' or
                 self.pages_processed < self.pages_limit
@@ -36,18 +32,15 @@ class WikiSpider(scrapy.Spider):
 
     def parse_movie(self, response):
         def get_clean_text(label):
-            # Ищем ячейку и достаем текст, ИГНОРИРУЯ теги <style> и пометки типа [вд]
             nodes = response.xpath(
                 f"//table[contains(@class, 'infobox')]//th[contains(., '{label}')]/following-sibling::td//text()[not(parent::style) and not(parent::sup)]"
             ).getall()
             return [n.strip() for n in nodes if n.strip() and n.strip() not in [',', '[', ']']]
 
-        # Название: если в инфобоксе "?" или пусто, берем чистый заголовок страницы h1
         title = response.css('th.infobox-above::text').get()
         if not title or title.strip() in ["?", "???", ""]:
             title = response.css('h1#firstHeading i::text').get() or response.css('h1#firstHeading::text').get()
 
-        # Год: собираем из всех возможных полей
         year_data = "".join(get_clean_text('Год') + get_clean_text('Выпуск') + get_clean_text('Дата выхода'))
         year_match = re.search(r'\d{4}', year_data)
         year = year_match.group(0) if year_match else None
@@ -58,4 +51,5 @@ class WikiSpider(scrapy.Spider):
             'Режиссер': ", ".join(get_clean_text('Режиссёр')),
             'Страна': ", ".join(get_clean_text('Страна')),
             'Год': year
+
         }
